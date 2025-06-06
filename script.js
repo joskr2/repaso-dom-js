0f52ff67e114658153f9e4c3240bdc7be1546e13
joskr2 <josretamozovar@gmail.com>
2025-06-05 19:59:03 -0500

    recuperacion de elementos del dom

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const campoTarea = document.getElementById("campoTarea");
+
+const botonAgregar = document.getElementById("botonAgregar");
+
+const listaTareas = document.getElementById("listaTareas");
+
+const contadorTareas = document.getElementById("contadorTareas");
+
+const estadoVacio = document.getElementById("estadoVacio");
+
+
+const encabezado = document.querySelector(".encabezado");
+
+const contenedor = document.querySelector(".contenedor-principal");
+
+
+const botonesFiltro = document.querySelectorAll(".boton-filtro");
+
+
